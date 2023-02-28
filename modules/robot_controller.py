39271ee0a3a02fd58b136d6ddc2b7593e967124c
AI-RYZEN1 <pascal.graf@hs-heilbronn.de>
2023-02-28 14:32:01 +0100

    robot controller example script

--- modules/robot_controller.py
+++ modules/robot_controller.py
@@ -1,3 +1,11 @@
+from cri.robot import AsyncRobot
+from cri_dobot.robot import SyncDobot
+from cri_dobot.controller import dobotMagicianController
+
+import time
+import numpy as np
+
+
 class DoBotRobotController:
     def __init__(self):
         # Find the correct USB port to connect to
@@ -158,3 +166,228 @@ def CalcRobotCoord(x_cam, y_cam):
     else:
         print('[Warning] The Coordinates are not calibrated')
         print('[INFO] Follow the instructions of the README.md')
+
+
+def main():
+    base_frame = (0, 0, 0, 0, 0, 0)
+    # base frame: x->front, y->right, z->up
+    work_frame = (260, 0, 80, 0, 0, 0)
+
+    robot = dobotMagicianController(port="COM3")
+    with AsyncRobot(SyncDobot(robot)) as robot:
+
+        speed_test1 = robot.linear_speed
+        print("speed_test1 (mm/s)", speed_test1)
+
+        angular_speed_test1 = robot.angular_speed
+        print("angular_speed_test1 (deg/s)", angular_speed_test1)
+
+        # Set TCP, linear speed,  angular speed and coordinate frame
+        # With a Tactip attachment and corresponding tool center point of (59.7, 0, 0, 0, 0, 0). This is the same tool center point as the suction cup or gripper
+        robot.tcp = (59.7, 0, 0, 0, 0, 0)
+        robot.linear_speed = 100
+        robot.angular_speed = 100
+
+        speed_test2 = robot.linear_speed
+        print("speed_test2 (mm/s)", speed_test2)
+
+        angular_speed_test2 = robot.angular_speed
+        print("angular_speed_test2 (deg/s)", angular_speed_test2)
+
+        # ----- TCP section
+        # print("For info only: TCP bug has been rectified and TCP is now working correctly!")
+        tcp_test2 = robot.tcp
+        print("tcp_test2", tcp_test2)
+
+        print("do something")
+
+        tcp_test3 = robot.tcp
+        print("tcp_test3", tcp_test3)
+
+        # ---- Start from below here !
+
+        # Display robot info
+        # print("Robot info: {}".format(robot.info)) #Currently not used
+
+        # Example of displaying current command index on dobot magician
+        currentIndex = robot.sync_robot.controller.current_index()
+        print("Current Command Index: {}".format(currentIndex))
+
+        # Set base frame for storing home position
+        robot.coord_frame = base_frame
+
+        # Set home position
+        print("Setting home position")
+        robot.sync_robot.set_home_params((220, 0, 80, 0, 0, 0))
+
+        # Perform homing
+        print("Starting homing")
+        robot.sync_robot.perform_homing()
+        print("Homing finished...")
+
+        # Return to work frame
+        robot.coord_frame = work_frame
+
+        # Display initial joint angles
+        print("Initial joint angles: {}".format(robot.joint_angles))
+
+        # Display initial pose in work frame
+        print("Initial pose in work frame: {}".format(robot.pose))
+
+        # Move to origin of work frame
+        print("Moving to origin of work frame ...")
+        robot.move_linear((0, 0, 0, 0, 0, 0))
+
+        # Increase and decrease all joint angles
+        print("Increasing and decreasing all joint angles ...")
+
+        newJointAngles = tuple(np.add(robot.joint_angles, (5, 5, 5, 5)))
+        robot.move_joints(newJointAngles)
+        print("Joint angles after increase: {}".format(robot.joint_angles))
+
+        newJointAngles = tuple(np.subtract(robot.joint_angles, (5, 5, 5, 5)))
+        robot.move_joints(newJointAngles)
+        print("Joint angles after decrease: {}".format(robot.joint_angles))
+
+        # # Move backward and forward
+        print("Moving backward and forward ...")
+        robot.move_linear((-20, 0, 0, 0, 0, 0))
+        robot.move_linear((0, 0, 0, 0, 0, 0))
+
+        # Move right and left
+        print("Moving right and left ...")
+        robot.move_linear((0, -20, 0, 0, 0, 0))
+        robot.move_linear((0, 0, 0, 0, 0, 0))
+
+        # Move down and up
+        print("Moving down and up ...")
+        robot.move_linear((0, 0, -20, 0, 0, 0))
+        robot.move_linear((0, 0, 0, 0, 0, 0))
+
+        # # Turn clockwise and anticlockwise around work frame z-axis
+        print("Turning clockwise and anticlockwise around work frame z-axis ...")
+        robot.move_linear((0, 0, 0, 0, 0, -100))
+        robot.move_linear((0, 0, 0, 0, 0, 0))
+
+        # Print Pose in this position
+        print("pose in work frame: {}".format(robot.pose))
+        # Print joint angles in this position
+        print("joint angles: {}".format(robot.joint_angles))
+
+        # Move to offset pose then tap down and up in sensor frame
+        print("Moving to 20 mm/deg offset in all pose dimensions ...")
+        robot.move_linear((-20, -20, -20, 0, 0, -20))
+        print("Pose after offset move: {}".format(robot.pose))
+        # THIS MOVEMENT ON THE DOBOT ARM IS UP THEN DOWN ->> CHECK WITH JOHN IF IMPORTANT :) !
+        print("Tapping down and up ...")
+        robot.coord_frame = base_frame
+        robot.coord_frame = robot.pose
+        robot.move_linear((0, 0, -20, 0, 0, 0))
+        robot.move_linear((0, 0, 0, 0, 0, 0))
+        robot.coord_frame = work_frame
+        print("Moving to origin of work frame ...")
+        robot.move_linear((0, 0, 0, 0, 0, 0))
+
+        # Pause before commencing asynchronous tests
+        print("Waiting for 5 secs ...")
+        time.sleep(5)
+        print("Repeating test sequence for asynchronous moves ...")
+
+        # Increase and decrease all joint angles (async)
+        print("Increasing and decreasing all joint angles ...")
+
+        newJointAngles = tuple(np.add(robot.joint_angles, (5, 5, 5, 5)))
+        robot.async_move_joints(newJointAngles)
+        print("Getting on with something else while command completes ...")
+        robot.async_result()
+        print("Joint angles after increase: {}".format(robot.joint_angles))
+
+        newJointAngles = tuple(np.subtract(robot.joint_angles, (5, 5, 5, 5)))
+        robot.async_move_joints(newJointAngles)
+        print("Getting on with something else while command completes ...")
+        robot.async_result()
+        print("Joint angles after decrease: {}".format(robot.joint_angles))
+
+        # Move backward and forward (async)
+        print("Moving backward and forward (async) ...")
+        robot.async_move_linear((-20, 0, 0, 0, 0, 0))
+        print("Getting on with something else while command completes ...")
+        robot.async_result()
+        robot.async_move_linear((0, 0, 0, 0, 0, 0))
+        print("Getting on with something else while command completes ...")
+        robot.async_result()
+
+        # Move right and left
+        print("Moving right and left (async) ...")
+        robot.async_move_linear((0, -20, 0, 0, 0, 0))
+        print("Getting on with something else while command completes ...")
+        robot.async_result()
+        robot.async_move_linear((0, 0, 0, 0, 0, 0))
+        print("Getting on with something else while command completes ...")
+        robot.async_result()
+
+        # Move down and up (async)
+        print("Moving down and up (async) ...")
+        robot.async_move_linear((0, 0, -20, 0, 0, 0))
+        print("Getting on with something else while command completes ...")
+        robot.async_result()
+        robot.async_move_linear((0, 0, 0, 0, 0, 0))
+        print("Getting on with something else while command completes ...")
+        robot.async_result()
+
+        # ---- Note that rolling is not possible on a 4dof robot and will return an error
+        # # Roll right and left (async)
+        # print("Rolling right and left (async) ...")
+        # robot.async_move_linear((0, 0, 0, 20, 0, 0))
+        # print("Getting on with something else while command completes ...")
+        # robot.async_result()
+        # robot.async_move_linear((0, 0, 0, 0, 0, 0))
+        # print("Getting on with something else while command completes ...")
+        # robot.async_result()
+
+        # # Roll forward and backward (async)
+        # print("Rolling forward and backward (async) ...")
+        # robot.async_move_linear((0, 0, 0, 0, 20, 0))
+        # print("Getting on with something else while command completes ...")
+        # robot.async_result()
+        # robot.async_move_linear((0, 0, 0, 0, 0, 0))
+        # print("Getting on with something else while command completes ...")
+        # robot.async_result()
+        # ----
+
+        # Turn clockwise and anticlockwise around work frame z-axis (async)
+        print("Turning clockwise and anticlockwise around work frame z-axis (async) ...")
+        robot.async_move_linear((0, 0, 0, 0, 0, -20))
+        print("Getting on with something else while command completes ...")
+        robot.async_result()
+        robot.async_move_linear((0, 0, 0, 0, 0, 0))
+        print("Getting on with something else while command completes ...")
+        robot.async_result()
+
+        # Move to offset pose then tap down and up in sensor frame (async)
+        print("Moving to 20 mm/deg offset in all pose dimensions (async) ...")
+        robot.async_move_linear((20, 20, 20, 0, 0, 20))
+        print("Getting on with something else while command completes ...")
+        robot.async_result()
+        print("Pose after offset move: {}".format(robot.pose))
+        print("Tapping down and up (async) ...")
+        robot.coord_frame = base_frame
+        robot.coord_frame = robot.pose
+        robot.async_move_linear((0, 0, -20, 0, 0, 0))
+        print("Getting on with something else while command completes ...")
+        robot.async_result()
+        robot.async_move_linear((0, 0, 0, 0, 0, 0))
+        print("Getting on with something else while command completes ...")
+        robot.async_result()
+        robot.coord_frame = work_frame
+        print("Moving to origin of work frame ...")
+        robot.async_move_linear((0, 0, 0, 0, 0, 0))
+        print("Getting on with something else while command completes ...")
+        robot.async_result()
+
+
+        print("Final pose in work frame: {}".format(robot.pose))
+
+
+if __name__ == '__main__':
+    main()
