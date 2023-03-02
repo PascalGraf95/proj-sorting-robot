f1c30568348e767e00f0300457fe9aa465f8b7bc
PascalGraf <pascal.graf@hs-heilbronn.de>
2023-03-02 15:27:32 +0100

    correct transformation

--- modules/misc.py
+++ modules/misc.py
@@ -4,7 +4,9 @@ import serial
 import serial.tools.list_ports
 import cv2
 import numpy as np
+
 c2r_matrix = np.zeros(0)
+r2c_matrix = np.zeros(0)
 
 
 def serial_ports():
@@ -18,24 +20,30 @@ def serial_ports():
 
 
 def transform_cam_to_robot(cam_coordinates):
-    print(np.matmul(cam_coordinates, c2r_matrix))
-def transform_robot_to_cam(robot_coordinates):
-    print(np.matmul(robot_coordinates, c2r_matrix))
+    print(np.matmul(c2r_matrix, cam_coordinates))
+    return np.matmul(c2r_matrix, cam_coordinates)
 
-def calc_transformation_matrix():
+
+def transform_robot_to_cam(robot_coordinates):
+    print(np.matmul(r2c_matrix, robot_coordinates))
+    return np.matmul(r2c_matrix, robot_coordinates)
+
+
+def calc_transformation_matrices():
     # Left Top: 228, 23 --> (-70, -40, -58, 0, 0, 0)
     # Left Bottom: 230, 357 --> (70, -40, -58, 0, 0, 0)
     # Right Top: 745, 15 --> (-70, 180, -58, 0, 0, 0)
     # Right Bottom: 750, 305--> (50, 180, -58, 0, 0, 0)
     cam_points = np.array([[228, 23], [230, 357], [745, 15]]).astype(np.float32)
     robot_points = np.array([[-70, -40], [70, -40], [-70, 180]]).astype(np.float32)
-    global c2r_matrix
+    global c2r_matrix, r2c_matrix
     c2r_matrix = cv2.getAffineTransform(cam_points, robot_points)
-    print(c2r_matrix)
-    return c2r_matrix
-
+    c2r_matrix = np.append(c2r_matrix, np.array([[0, 0, 1]]), axis=0)
+    r2c_matrix = np.linalg.inv(c2r_matrix)
+    return c2r_matrix, r2c_matrix
 
 
 if __name__ == '__main__':
-    calc_transformation_matrix()
-    transform_cam_to_robot(np.array([228, 23]))
+    calc_transformation_matrices()
+    transform_cam_to_robot(np.array([228, 23, 1]))
+    transform_robot_to_cam(np.array([-70, 180, 1]))
