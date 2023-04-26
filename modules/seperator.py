748811ad6ac159dfc686af11d6d623cf40f91101
Dome <131344912+Doetty@users.noreply.github.com>
2023-04-26 15:23:39 +0200

    Changed the -gitinore and added .ino code for seperator

--- modules/seperator.py
+++ modules/seperator.py
@@ -2,7 +2,6 @@ from modules.misc import serial_ports
 import serial
 import time
 
-
 class Seperator:
     def __init__(self):
         # Establish a serial connection to the arduino
@@ -14,17 +13,20 @@ class Seperator:
                 break
         if arduino_port == "":
             raise ConnectionError("[ERROR] Seperator port could not be found!")
+
         self.serial_connection = serial.Serial(arduino_port, 9600)
         self.running = False
         time.sleep(2)  # Seems necessary for serial connection to establish correctly
 
     def start(self):
+        print("Start Seperator")
         self.running = True
-        self.serial_connection.write(b'\x01')
+        self.serial_connection.write(0x01)
 
     def stop(self):
+        print("Stop Seperator")
         self.running = False
-        self.serial_connection.write(b'\x02')
+        self.serial_connection.write(0x02)
 
     def disconnect(self):
         self.running = False
@@ -36,3 +38,5 @@ if __name__ == '__main__':
     seperator.start()
     time.sleep(5)
     seperator.stop()
+    time.sleep(5)
+    seperator.start()
