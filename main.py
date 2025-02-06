756b8e50bf05187b311df8095e3cda0b706f7e07
SvenTizian <sven.tizian@googlemail.com>
2025-02-06 16:18:48 +0100

    bg subtractor and video capture
    fixed
    changed to use the correct fps and path

--- main.py
+++ main.py
@@ -81,15 +81,13 @@ def parse_and_preprocess_features(feature_method="cv_image_features", feature_ty
 
 
 def test_camera_image(cam):
-
-    print("[DEBUG] Status: Connecting to Conveyor")
+    print("[DEBUG] Status: test_camera_image")
     conveyor_belt = ConveyorBelt()
+    conveyor_belt.stop()
     conveyor_belt.start()
     time.sleep(5)
     print(cam.width, cam.height)
-    out = cv2.VideoWriter('background_video.avi', cv2.VideoWriter_fourcc(*'XVID'), 30, (int(cam.width), int(cam.height)))
-    recording_duration = 120
-    start_time = time.time()
+
     while True:
         image = cam.capture_image()
         preprocessed_image = image_preprocessing(image, LenseType.NEW_LENS)
@@ -103,11 +101,7 @@ def test_camera_image(cam):
         if frame is None:
             print("[DEBUG] No frame detected")
             break
-        out.write(frame)
-        if time.time() - start_time >= recording_duration:
-            print("[DEBUG] Recording finished")
-            conveyor_belt.stop()
-            sys.exit()
+
         if show_image(canvas_image, wait_for_ms=1):
             break
         if show_image(preprocessed_image2, wait_for_ms=1, window_name="Image2"):
@@ -189,6 +183,7 @@ def calibrate_robot():
     cam = IDSCameraController()
     cam.capture_image()
     time.sleep(0.5)
+    #video_capture(cam)
     while True:
         #robot.test_robot()
         test_camera_image(cam)
